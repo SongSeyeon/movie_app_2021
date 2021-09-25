721136a655852f6c5b96631e304ccefb65a8a24e
SongSeyeon <songsy405@pusan.ac.kr>
2021-09-25 19:48:26 +0900

    css

--- src/App.js
+++ src/App.js
@@ -1,78 +1,54 @@
 import PropTypes from "prop-types";
+import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
 
-function Food({name, picture, rating}){
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}</h4>
-      <img src={picture} alt={name}/>
-    </div>
-    );
-}
-
-Food.propTypes={
-  name : PropTypes.string.isRequired,
-  picture : PropTypes.string.isRequired,
-  rating : PropTypes.number.isRequired, //number 자료형이 아닐 경우 에러 발생
-};
-
-const foodLike=[
-  {
-    id:1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-    rating:5.0,
-  },
-  {
-    id:2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-    rating:4.9,
-  },
-  {
-    id:3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-    rating:4.8,
-  },
-  {
-    id:4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-    rating:5.5,
-  },
-  {
-    id:5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-    rating:4.7,
+  getMovies = async () => {  //비동기적 함수 실행을 위함(async-await)
+    const { data: { data: { movies } } } = await axios.get("https://yts-proxy.now.sh/list_movies.json");    //가져올 API의 URL
+    //axios.get() 메소드가 완료될 때까지 기다린다.
+    console.log(movies); //movies.data.data.movies라고 할 걸 단축시켜준다.
+    this.setState({ movies: movies, isLoading: false });
   }
-];
 
-function renderFood(dish){
-  return <Food name={dish.name} picture={dish.image} rating={dish.rating} />
+  componentDidMount() {  //render된 직후 호출되는 component life cycle
+    this.getMovies();
+  }
+
+  render() {
+    const { isLoading, movies } = this.state;   //isLoading을 사용할 때마다 this.state.을 매번 붙이지 않기 위해
+    return (
+    <section className="container">
+      {isLoading ?(
+        <div className="loader">
+          <span className="loader__text">Loading...</span>
+        </div>
+      ) : (
+        <div className="movies">
+          {movies.map(movie => (
+          <Movie
+            key={movie.id}
+            id={movie.id}
+            year={movie.id}
+            title={movie.title}
+            summary={movie.summary}
+            poster={movie.medium_cover_image}
+            genres={movie.genres}
+          />
+        ))}
+        </div>
+        
+    )}
+    </section>);
+  }
 }
 
-function App() {
-  return (
-    <div className="App">
-      {console.log(foodLike.map(renderFood))}
-      {foodLike.map(dish=>(
-        <Food
-          key={dish.id}
-          name={dish.name}
-          picture={dish.image}
-          rating={dish.rating}
-        />
-      ))}
-    </div>
-  );
-}
+
 
 export default App;
